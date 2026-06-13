8eb7596640b1618dffd0ad17512bdef2768e27ae
JuliaVicentini <doamaralju@gmail.com>
2026-06-13 14:15:14 -0300

    Remove componentes temporarios de jogador

--- backend/src/services/jogador_service.py
+++
@@ -1,9 +0,0 @@
-from repositories.jogador_repository import JogadorRepository
-
-class JogadorService:
-
-    def __init__(self):
-        self.repository = JogadorRepository()
-
-    def listar_jogadores(self):
-        return self.repository.listar_todos()
