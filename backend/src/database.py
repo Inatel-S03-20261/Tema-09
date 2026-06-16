b26465499c25574ad680a3618f3159380c434653
JuliaVicentini <doamaralju@gmail.com>
2026-06-15 22:29:04 -0300

    Implementa Singleton para conexao MySQL

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,9 +1,20 @@
 import mysql.connector
 
+class DatabaseConnection:
+    _instance = None
+
+    @staticmethod
+    def get_instance():
+        if DatabaseConnection._instance is None:
+            DatabaseConnection._instance = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="root",
+                database="pokedex_db"
+            )
+
+        return DatabaseConnection._instance
+
+
 def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="pokedex_db"
-    )
+    return DatabaseConnection.get_instance()
